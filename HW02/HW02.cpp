c7e26db74e8706a6428a29f025765329a1cfd25c
DiooN <dioon0000@naver.com>
2025-05-29 15:22:37 +0900

    필수 기능 구현

--- HW02/HW02.cpp
+++ HW02/HW02.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+class Animal{
+public:
+    virtual void makeSound()=0; //순수 가상 함수
+};
+class Dog : public Animal{
+public:
+    void makeSound() override{
+        cout << "Mung~Mung~\n";
+    }
+};
+class Cat : public Animal{
+    void makeSound() override{
+        cout << "Aeow~Aeow~\n";
+    }
+};
+class Cow : public Animal{
+    void makeSound() override{
+        cout << "Moo~~\n";
+    }
+};
 int main(){
+    Animal* arr[3];
+    arr[0] = new Dog();
+    arr[1] = new Cat();
+    arr[2] = new Cow();
+
+    for(Animal* a : arr){
+        a->makeSound();
+    }
+
+    delete arr[0];
+    delete arr[1];
+    delete arr[2];
     return 0;
 }
